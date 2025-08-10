5c467168e4e7f703a9aab8eddee51532b6347134
Myloslava <myloslava.martynova@gmail.com>
2025-08-10 13:17:16 +0200

    change url for mm-003 and for mm-005

--- tests/nasa-api/nasa-api.spec.ts
+++ tests/nasa-api/nasa-api.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 const API_KEY = "JzaeM8dMrWZzUG3ZdpLQa2epUNbO13aSFosayUY4";
 
-test("get info with date parameter", async ({ request }) => {
+test("MMN-001, get info with date parameter", async ({ request }) => {
   const response = await request.get(
     `/planetary/apod?api_key=${API_KEY}&date=2025-08-01`
   );
@@ -16,18 +16,22 @@ test("get info with date parameter", async ({ request }) => {
   expect(headers["x-ratelimit-limit"]).toBe("4000");
 });
 
-test("get with data and thumbs ", { tag: ["@smoke"] }, async ({ request }) => {
-  const params = {
-    api_key: API_KEY,
-    thumbs: true,
-  };
-  const responce = await request.get(
-    `/planetary/apod?api_key=${API_KEY}&thumbs=true`
-  );
-  const json = await responce.json();
-  expect(responce.status()).toBe(200);
-});
-test("get with start and end dates", async ({ request }) => {
+test(
+  "MMN-002, get with data and thumbs ",
+  { tag: ["@smoke"] },
+  async ({ request }) => {
+    const params = {
+      api_key: API_KEY,
+      thumbs: true,
+    };
+    const responce = await request.get(
+      `/planetary/apod?api_key=${API_KEY}&thumbs=true`
+    );
+    const json = await responce.json();
+    expect(responce.status()).toBe(200);
+  }
+);
+test("MMN-003, get with start and end dates", async ({ request }) => {
   const start_date = "2025-08-01";
   const end_date = "2025-08-03";
   const responce = await request.get(
@@ -43,7 +47,7 @@ test("get with start and end dates", async ({ request }) => {
   expect(dates[1]).toBe("2025-08-02");
   expect(dates[2]).toBe("2025-08-03");
 });
-test("get data with no parameters", async ({ request }) => {
+test("MMN-004, get data with no parameters", async ({ request }) => {
   const responce = await request.get(`/`);
   expect(responce.status()).toBe(200);
 });
